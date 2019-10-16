7d416de6a3ab80fd7385bcba82bf31e826f22922
FranMacedo <54102462+FranMacedo@users.noreply.github.com>
2019-10-16 16:54:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,11 @@ FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, FONT_AWESOME]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, 
+                meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ],
+               )
 # para prevenir erros
 app.config['suppress_callback_exceptions'] = True
 server = app.server
